a6f2b37193663d3af7fadcefaf1337a2c7654806
Yatytbidlo <prisilaite.suda@gmail.com>
2023-07-21 12:30:21 +0600

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from "../Button/Button";
 import "./Header.css"
-import "./Button/Button.css"
 
 const Header = () => {
     const tg = window.Telegram.WebApp
